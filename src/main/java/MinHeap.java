e0322f421b3aef173e215a6ced42e0dab89597ee
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-10 13:22:15 +0200

    Create constructor and attributes in MinHeap class

--- src/main/java/MinHeap.java
+++ src/main/java/MinHeap.java
@@ -1,2 +1,11 @@
 public class MinHeap {
+
+    private int capacity;
+    private int size = 0;
+    private int[] array;
+
+    public MinHeap(int capacity) {
+        this.capacity = capacity;
+        this.array = new int[capacity];
+    }
 }
