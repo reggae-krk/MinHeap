d9bf27018b43a7d4184be02e67abca778c53a40e
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-10 14:44:25 +0200

    Copy all methods that are the same as in MinHeap class

--- src/main/java/MaxHeap.java
+++ src/main/java/MaxHeap.java
@@ -1,2 +1,88 @@
+import java.util.Arrays;
+
 public class MaxHeap {
+
+    private int capacity;
+    private int size = 0;
+    private int[] array;
+
+    public MaxHeap(int capacity) {
+        this.capacity = capacity;
+        this.array = new int[capacity];
+    }
+
+    private int getLeftChildIndex(int parentIndex) {
+        return 2 * parentIndex + 1;
+    }
+
+    private int getRightChildIndex(int parentIndex) {
+        return 2 * parentIndex + 2;
+    }
+
+    private int getParentIndex(int childIndex) {
+        if (childIndex == 0) return 0;
+        return (childIndex - 1) / 2;
+    }
+
+    private boolean hasLeftChild(int index) {
+        return getLeftChildIndex(index) < size;
+    }
+
+    private boolean hasRightChild(int index) {
+        return getRightChildIndex(index) < size;
+    }
+
+    private boolean hasParent(int index) {
+        return getParentIndex(index) >= 0;
+    }
+
+    private int getLeftChild(int index) {
+        return this.array[getLeftChildIndex(index)];
+    }
+
+    private int getRightChild(int index) {
+        return this.array[getRightChildIndex(index)];
+    }
+
+    private int getParent(int index) {
+        return this.array[getParentIndex(index)];
+    }
+
+    private void swap(int indexOne, int indexTwo) {
+        int temp = this.array[indexOne];
+        this.array[indexOne] = this.array[indexTwo];
+        this.array[indexTwo] = temp;
+    }
+
+    private void doubleCapacity() {
+        if (this.size == this.capacity) {
+            this.array = Arrays.copyOf(this.array, this.capacity * 2);
+            this.capacity *= 2;
+        }
+    }
+
+    public int peek() {
+        if (this.size == 0) throw new IllegalStateException();
+        return this.array[0];
+    }
+
+    public int pool() {
+        if (this.size == 0) throw new IllegalStateException();
+        int elementToRemove = this.array[0];
+        this.array[0] = this.array[size - 1];
+        this.size--;
+        heapifyDown();
+        return elementToRemove;
+    }
+
+    public void add(int element) {
+        doubleCapacity();
+        this.array[size] = element;
+        this.size++;
+        heapifyUp();
+    }
+
+    public int[] getArray() {
+        return array;
+    }
 }
